5223ef9a88eb2bb6c7ae306f004ead09f264f7c7
urbutt <137639766+urbutt@users.noreply.github.com>
2024-08-13 01:43:48 +0530

    "Added comments to User class

--- User.java
+++ User.java
@@ -1,15 +1,22 @@
 import java.io.Serializable;
 
+/*
+ * The User class represents a user entity in the system.
+ * It implements Serializable for object serialization.
+ */
 public class User implements Serializable {
+    // The class uses a serialVersionUID for version control during serialization
     private static final long serialVersionUID = 1L;
     private String Name;
     private String PasswordHash;
 
+    // Constructor creates a new User object with the specified name and password
     public User(String name, String passwordHash) {
         this.Name = name;
         this.PasswordHash = passwordHash;
     }
 
+    // getter methods for accessing user information
     public String getName() {
         return Name;
     }
